39dff3ca1bc1546babc76cb696136d1db1d979fe
Botir Mahmudov <botir6@gmail.com>
2026-04-04 04:51:41 +0500

    Add files via upload

--- run_admin.py
+++ run_admin.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import sys
+
+ROOT = Path(__file__).resolve().parent
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
 from app.admin.app import create_app
 from app.config import settings
 
