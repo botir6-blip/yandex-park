c419f6394edfa0b928e81e8148bc23ba80fcc64b
Botir Mahmudov <botir6@gmail.com>
2026-04-04 17:16:42 +0500

    Update run_admin.py

--- run_admin.py
+++ run_admin.py
@@ -1,5 +1,9 @@
 from app.admin.app import create_app
 from app.config import settings
+from app.db import Base, engine
+import app.models  # моделлар Base га рўйхатдан ўтиши учун
+
+Base.metadata.create_all(bind=engine)
 
 app = create_app()
 
