9de91338fb617aa94a18c56fa2c607bd54c1d269
Botir Mahmudov <botir6@gmail.com>
2026-04-04 16:09:54 +0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,14 +1,20 @@
-from contextlib import contextmanager
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
+from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 from app.config import settings
 
 if not settings.database_url:
     raise RuntimeError("DATABASE_URL бўш. .env файлини тўлдиринг.")
 
-engine = create_engine(settings.database_url, future=True, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+db_url = settings.database_url.strip()
+
+if db_url.startswith("postgresql+psycopg2://"):
+    db_url = db_url.replace("postgresql+psycopg2://", "postgresql+psycopg://", 1)
+elif db_url.startswith("postgresql://"):
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
+
+engine = create_engine(db_url, future=True, pool_pre_ping=True)
+SessionLocal = scoped_session(sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True))
+db_session = SessionLocal
 Base = declarative_base()
 
 
